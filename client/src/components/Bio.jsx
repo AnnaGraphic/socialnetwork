c1a20ff3ec2ee5cd2b84707ca35ffa2262f3a5b6
annar <p4nd4@posteo.de>
2022-12-04 01:00:35 +0100

    Bio.jsx: new Component | add and edit user bio

--- client/src/components/Bio.jsx
+++ client/src/components/Bio.jsx
@@ -1,12 +1,79 @@
-function Bio(props) {
-    return (
-        <div>
-            <p>bio</p>
+import { Component } from "react";
+export default class Bio extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editing: false,
+        };
+        this.editBio = this.editBio.bind(this);
+        // this.editBio = () => this.editBio();
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    // +++ methods +++
+    editBio() {
+        this.setState({ editing: true });
+    }
+    handleInputChange(e) {
+        //console.log(e);
+        const text = e.currentTarget.value;
+        this.setState({
+            [e.currentTarget.name]: text,
+        });
+    }
+
+    handleSubmit() {
+        console.log("bio handle submit", this.state.inputBio);
+        fetch("/editbio", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ inputBio: this.state.inputBio }),
+        })
+            .then((res) => res.json())
+            .then((response) => {
+                console.log("response handle submit", response);
+                if (response.success) {
+                    //"success true"
+                    this.setState({ editing: false });
+                    //
+                    location.reload();
+                } else {
+                    console.log("response handle submit 2", response);
+                }
+            })
+            .catch((err) => {
+                console.log("registration", err.message);
+            });
+    }
+
+    render() {
+        const bio = this.props.user?.bio || "";
+        return (
             <div>
-                <input type="text" placeholder="add a bio"></input>
-                <button>edit bio</button>
+                <p>bio</p>
+                <div>
+                    {!this.state.editing && (
+                        <div>
+                            <div className="biotext">{bio}</div>
+                            <button onClick={this.editBio}>edit bio</button>
+                        </div>
+                    )}
+                    {this.state.editing && (
+                        <div>
+                            <input
+                                type="text"
+                                name="inputBio"
+                                onChange={this.handleInputChange}
+                                placeholder="add a bio"
+                            ></input>
+                            <button onClick={(e) => this.handleSubmit(e)}>
+                                save
+                            </button>
+                        </div>
+                    )}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
-export default Bio;
