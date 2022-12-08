0bf096da2438a02eea483f179218e53cb45f8ba7
annar <p4nd4@posteo.de>
2022-12-08 18:02:47 +0100

    implementing redux  basics

---
+++ client/src/redux/rootReducer.js
@@ -0,0 +1,19 @@
+// const initialState = {
+//     bioText: "",
+// };
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({});
+
+// function rootReducer(state = initialState, action) {
+//     // if (action.type === "bioUpdated") {
+//     //     return {
+//     //         ...state,
+//     //         bioText: AuthenticatorAssertionResponse.payload,
+//     //     };
+//     // }
+//     // // in any case: return the STATE!
+//     // return state;
+// }
+
+export default rootReducer;
--- client/src/start.js
+++ client/src/start.js
@@ -2,6 +2,16 @@ import { createRoot } from "react-dom/client";
 import Welcome from "./components/Welcome";
 import App from "./components/App";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import * as immutableState from "redux-immutable-state-invariant";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./redux/rootReducer";
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(immutableState.default()))
+);
+
 const root = createRoot(document.querySelector("main"));
 
 fetch("/user/id.json")
@@ -11,7 +21,12 @@ fetch("/user/id.json")
         // the userId comes from the session on the server!
         // this means: the user is currently signed in!
         if (data.userId) {
-            root.render(<App />);
+            root.render(
+                <Provider store={store}>
+                    {" "}
+                    <App />{" "}
+                </Provider>
+            );
         } else {
             root.render(<Welcome />);
         }
