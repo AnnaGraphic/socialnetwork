968d0b84cd488a7011da737fa9bfdf5800348fa0
annar <p4nd4@posteo.de>
2022-12-13 19:58:43 +0100

    layot

--- client/src/components/Contacts/Contacts.jsx
+++ client/src/components/Contacts/Contacts.jsx
@@ -36,43 +36,53 @@ export default function Contacts() {
 
     return (
         <>
-            <h6>contacts</h6>
-            <ul>
-                {contacts &&
-                    contacts?.map((panda) => {
-                        console.log("panda after map", panda);
-                        return (
-                            <li key={panda.id}>
-                                <h5>{`${panda.first_name} ${panda.last_name}`}</h5>
-                                <img
-                                    src={`${
-                                        panda.profilepic_url ||
-                                        "/default_usericon_1.png"
-                                    }`}
-                                    alt=""
-                                />
-                            </li>
-                        );
-                    })}
-            </ul>
-            <h6>contact requests</h6>
-            <ul>
-                {requests &&
-                    requests?.map((panda) => {
-                        return (
-                            <li key={panda.id}>
-                                <h5>{`${panda.first_name} ${panda.last_name}`}</h5>
-                                <img
-                                    src={`${
-                                        panda.profilepic_url ||
-                                        "/default_usericon_1.png"
-                                    }`}
-                                    alt=""
-                                />
-                            </li>
-                        );
-                    })}
-            </ul>
+            <div className="contacts">
+                <h5>contacts</h5>
+                <ul>
+                    {contacts &&
+                        contacts?.map((panda) => {
+                            console.log("panda after map", panda);
+                            return (
+                                <li key={panda.id}>
+                                    <p>{`${panda.first_name} ${panda.last_name}`}</p>
+                                    <div className="profilePic">
+                                        <Link to={`/userprofile/${panda.id}`}>
+                                            <img
+                                                src={`${
+                                                    panda.profilepic_url ||
+                                                    "/default_usericon_1.png"
+                                                }`}
+                                                alt=""
+                                            />
+                                        </Link>
+                                    </div>
+                                </li>
+                            );
+                        })}
+                </ul>
+                <h5>contact requests</h5>
+                <ul>
+                    {requests &&
+                        requests?.map((panda) => {
+                            return (
+                                <li key={panda.id}>
+                                    <p>{`${panda.first_name} ${panda.last_name}`}</p>
+                                    <div>
+                                        <Link to={`/userprofile/${panda.id}`}>
+                                            <img
+                                                src={`${
+                                                    panda.profilepic_url ||
+                                                    "/default_usericon_1.png"
+                                                }`}
+                                                alt=""
+                                            />
+                                        </Link>
+                                    </div>
+                                </li>
+                            );
+                        })}
+                </ul>
+            </div>
         </>
     );
 }
